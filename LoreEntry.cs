37130f55ffc34b947b363dac35e92095276d40d7
Kalfadda <38150569+Kalfadda@users.noreply.github.com>
2024-07-07 12:09:20 -0700

    Delete LoreEntry.cs
    optimized scripts

--- LoreEntry.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LoreEntry", menuName = "GPT Hint System/Lore Entry")]
-public class LoreEntry : ScriptableObject
-{
-    public string key;
-    [TextArea(3, 10)]
-    public string description;
-}
