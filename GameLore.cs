928dfb93da67504d2df22eeadd2b06a67113a457
Kalfadda <38150569+Kalfadda@users.noreply.github.com>
2024-07-07 12:09:09 -0700

    Delete GameLore.cs

--- GameLore.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "GameLore", menuName = "GPT Hint System/Game Lore")]
-public class GameLore : ScriptableObject
-{
-    public List<LoreEntry> loreEntries = new List<LoreEntry>();
-}
